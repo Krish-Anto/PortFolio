72097fe21950d4fad03c8bdbf45e6e8517ddabfa
Krish-Anto <krishnarajan1995@gmail.com>
2025-02-03 13:29:52 +0530

    added Resume

---
+++ public/script.js
@@ -0,0 +1,5 @@
+function openResume() {
+    // Open the resume PDF file from the local storage/public folder
+    const resumeUrl = '/Krishnaraj.pdf';  // Update with the correct path to your resume
+    window.open(resumeUrl, '_blank');
+  }
